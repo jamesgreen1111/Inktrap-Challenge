20580d8950ef326c4dc357fdf1e8f71cfc9fdb4f
jamesgreen1111 <57545233+jamesgreen1111@users.noreply.github.com>
2021-06-30 20:26:17 +0100

    initial commit

---
+++ Inktrap-Assessment-QuestionsSite/src/js/Index.js
@@ -0,0 +1,172 @@
+const QuestionOne = document.getElementById("QuestionOne");
+
+function myFunctionOne() {
+    var toggleOne = document.getElementById("toggleOne");
+    var minus = document.getElementById("MinusOne");
+    var plus = document.getElementById("PlusOne");
+    if (toggleOne.style.display === "none") {
+        toggleOne.style.display = "block";
+        minus.style.display = "block";
+        plus.style.display = "none";
+        toggleOne.innerHTML = `<p id="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+      incididunt ut labore et dolore magna aliqua. Tempus urna et pharetra pharetra 
+      massa.<br><br>Ut morbi tincidunt augue interdum velit euismod in. Diam sit amet nisl 
+      suscipit adipiscing. In nulla posuere sollicitudin aliquam ultrices. Euismod quis 
+      viverra nibh cras pulvinar mattis nunc. Sodales neque sodales ut etiam sit. Ut 
+      lectus arcu bibendum at varius vel.<br><br>Laoreet sit amet cursus sit amet. Nisl vel 
+      pretium lectus quam id leo in vitae turpis. Vestibulum sed arcu non odio. 
+      Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Elementum 
+      facilisis leo vel fringilla. Porttitor rhoncus dolor purus non. Quis imperdiet 
+      massa tincidunt nunc. Aliquet sagittis id consectetur purus.</p>
+      <div class="chatButton">
+      <img src="../../Imgs/Bubble.png" height="12px" />
+      <p>Start a live chat</p>
+      </div>`
+    } else {
+        toggleOne.style.display = "none";
+        minus.style.display = "none";
+        plus.style.display = "block";
+    }
+  }
+
+  function myFunctionTwo() {
+      var toggleTwo = document.getElementById("toggleTwo");
+      var minus = document.getElementById("MinusTwo");
+      var plus = document.getElementById("PlusTwo");
+      if (toggleTwo.style.display === "none") {
+          toggleTwo.style.display = "block";
+          minus.style.display = "block";
+          plus.style.display = "none";
+        toggleTwo.innerHTML = `<p id="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+      incididunt ut labore et dolore magna aliqua. Tempus urna et pharetra pharetra 
+      massa.<br><br>Ut morbi tincidunt augue interdum velit euismod in. Diam sit amet nisl 
+      suscipit adipiscing. In nulla posuere sollicitudin aliquam ultrices. Euismod quis 
+      viverra nibh cras pulvinar mattis nunc. Sodales neque sodales ut etiam sit. Ut 
+      lectus arcu bibendum at varius vel.<br><br>Laoreet sit amet cursus sit amet. Nisl vel 
+      pretium lectus quam id leo in vitae turpis. Vestibulum sed arcu non odio. 
+      Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Elementum 
+      facilisis leo vel fringilla. Porttitor rhoncus dolor purus non. Quis imperdiet 
+      massa tincidunt nunc. Aliquet sagittis id consectetur purus.</p>
+      <div class="chatButton">
+      <img src="../../Imgs/Bubble.png" height="12px" />
+      <p>Start a live chat</p>
+      </div>`
+    } else {
+      toggleTwo.style.display = "none";
+      minus.style.display = "none";
+      plus.style.display = "block";
+    }
+  }
+  function myFunctionThree() {
+    var toggleThree = document.getElementById("toggleThree");
+    var minus = document.getElementById("MinusThree");
+    var plus = document.getElementById("PlusThree");
+    if (toggleThree.style.display === "none") {
+        toggleThree.style.display = "block";
+        minus.style.display = "block";
+        plus.style.display = "none";
+        toggleThree.innerHTML = `<p id="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+      incididunt ut labore et dolore magna aliqua. Tempus urna et pharetra pharetra 
+      massa.<br><br>Ut morbi tincidunt augue interdum velit euismod in. Diam sit amet nisl 
+      suscipit adipiscing. In nulla posuere sollicitudin aliquam ultrices. Euismod quis 
+      viverra nibh cras pulvinar mattis nunc. Sodales neque sodales ut etiam sit. Ut 
+      lectus arcu bibendum at varius vel.<br><br>Laoreet sit amet cursus sit amet. Nisl vel 
+      pretium lectus quam id leo in vitae turpis. Vestibulum sed arcu non odio. 
+      Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Elementum 
+      facilisis leo vel fringilla. Porttitor rhoncus dolor purus non. Quis imperdiet 
+      massa tincidunt nunc. Aliquet sagittis id consectetur purus.</p>
+      <div class="chatButton">
+      <img src="../../Imgs/Bubble.png" height="12px" />
+      <p>Start a live chat</p>
+      </div>`
+    } else {
+      toggleThree.style.display = "none";
+      minus.style.display = "none";
+      plus.style.display = "block";
+    }
+  }
+  function myFunctionFour() {
+    var toggleFour = document.getElementById("toggleFour");
+    var minus = document.getElementById("MinusFour");
+    var plus = document.getElementById("PlusFour");
+    if (toggleFour.style.display === "none") {
+        toggleFour.style.display = "block";
+        minus.style.display = "block";
+        plus.style.display = "none";
+        toggleFour.innerHTML = `<p id="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+      incididunt ut labore et dolore magna aliqua. Tempus urna et pharetra pharetra 
+      massa.<br><br>Ut morbi tincidunt augue interdum velit euismod in. Diam sit amet nisl 
+      suscipit adipiscing. In nulla posuere sollicitudin aliquam ultrices. Euismod quis 
+      viverra nibh cras pulvinar mattis nunc. Sodales neque sodales ut etiam sit. Ut 
+      lectus arcu bibendum at varius vel.<br><br>Laoreet sit amet cursus sit amet. Nisl vel 
+      pretium lectus quam id leo in vitae turpis. Vestibulum sed arcu non odio. 
+      Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Elementum 
+      facilisis leo vel fringilla. Porttitor rhoncus dolor purus non. Quis imperdiet 
+      massa tincidunt nunc. Aliquet sagittis id consectetur purus.</p>
+      <div class="chatButton">
+      <img src="../../Imgs/Bubble.png" height="12px" />
+      <p>Start a live chat</p>
+      </div>`
+    } else {
+      toggleFour.style.display = "none";
+      minus.style.display = "none";
+      plus.style.display = "block";
+    }
+  }
+  function myFunctionFive() {
+    var toggleFive = document.getElementById("toggleFive");
+    var minus = document.getElementById("MinusFive");
+    var plus = document.getElementById("PlusFive");
+    if (toggleFive.style.display === "none") {
+        toggleFive.style.display = "block";
+        minus.style.display = "block";
+        plus.style.display = "none";
+        toggleFive.innerHTML = `<p id="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+      incididunt ut labore et dolore magna aliqua. Tempus urna et pharetra pharetra 
+      massa.<br><br>Ut morbi tincidunt augue interdum velit euismod in. Diam sit amet nisl 
+      suscipit adipiscing. In nulla posuere sollicitudin aliquam ultrices. Euismod quis 
+      viverra nibh cras pulvinar mattis nunc. Sodales neque sodales ut etiam sit. Ut 
+      lectus arcu bibendum at varius vel.<br><br>Laoreet sit amet cursus sit amet. Nisl vel 
+      pretium lectus quam id leo in vitae turpis. Vestibulum sed arcu non odio. 
+      Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Elementum 
+      facilisis leo vel fringilla. Porttitor rhoncus dolor purus non. Quis imperdiet 
+      massa tincidunt nunc. Aliquet sagittis id consectetur purus.</p>
+      <div class="chatButton">
+      <img src="../../Imgs/Bubble.png" height="12px" />
+      <p>Start a live chat</p>
+      </div>`
+    } else {
+      toggleFive.style.display = "none";
+      minus.style.display = "none";
+      plus.style.display = "block";
+    }
+  }
+  function myFunctionSix() {
+    var toggleSix = document.getElementById("toggleSix");
+    var minus = document.getElementById("MinusSix");
+    var plus = document.getElementById("PlusSix");
+    if (toggleSix.style.display === "none") {
+        toggleSix.style.display = "block";
+        minus.style.display = "block";
+        plus.style.display = "none";
+        toggleSix.innerHTML = `<p id="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+      incididunt ut labore et dolore magna aliqua. Tempus urna et pharetra pharetra 
+      massa.<br><br>Ut morbi tincidunt augue interdum velit euismod in. Diam sit amet nisl 
+      suscipit adipiscing. In nulla posuere sollicitudin aliquam ultrices. Euismod quis 
+      viverra nibh cras pulvinar mattis nunc. Sodales neque sodales ut etiam sit. Ut 
+      lectus arcu bibendum at varius vel.<br><br>Laoreet sit amet cursus sit amet. Nisl vel 
+      pretium lectus quam id leo in vitae turpis. Vestibulum sed arcu non odio. 
+      Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Elementum 
+      facilisis leo vel fringilla. Porttitor rhoncus dolor purus non. Quis imperdiet 
+      massa tincidunt nunc. Aliquet sagittis id consectetur purus.</p>
+      <div class="chatButton">
+      <img src="../../Imgs/Bubble.png" height="12px" />
+      <p>Start a live chat</p>
+      </div>`
+    } else {
+      toggleSix.style.display = "none";
+      minus.style.display = "none";
+      plus.style.display = "block";
+    }
+  }
+
